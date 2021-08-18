4a3b090111150453613762fb8a7515ee4a1c12fd
Chiraag-Seth <77068249+Chiraag-Seth@users.noreply.github.com>
2021-08-18 18:12:59 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+  var firebase
+  var firebaseConfig = {
+    apiKey: "AIzaSyA8kJ_Jx6b2dWnVBVNTW11BVsfLHKCMl8A",
+    authDomain: "spectagram-dcc2f.firebaseapp.com",
+    databaseURL: "https://spectagram-dcc2f-default-rtdb.firebaseio.com",
+    projectId: "spectagram-dcc2f",
+    storageBucket: "spectagram-dcc2f.appspot.com",
+    messagingSenderId: "721075892456",
+    appId: "1:721075892456:web:dac19bbaa9e57ff7ec195d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
